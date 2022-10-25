69f1ac2e968ffe3939a285598678ad129cc7c922
Avdunusinghe <avdunusinghe@gmail.com>
2022-10-25 13:11:15 +0530

    Bug Fixed And Add React App

--- src/Web/ProcurementTracker.WebAPI/Program.cs
+++ src/Web/ProcurementTracker.WebAPI/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.WebHost.UseSentry();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
