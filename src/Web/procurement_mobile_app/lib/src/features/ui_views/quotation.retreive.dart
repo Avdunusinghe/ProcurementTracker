357ba1cc12b49badd35ba7743395d14c320a3e91
ChanduniNethmini <nchanduni21@gmail.com>
2022-11-04 19:52:49 +0530

    Implement get all orders in mobile ui

---
+++ src/Web/procurement_mobile_app/lib/src/features/ui_views/quotation.retreive.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/cupertino.dart';
+import 'package:procurement_mobile_app/src/features/procurement_app_theme.dart';
+
+class QutationRetrive extends StatelessWidget{
+  final AnimationController? animationController;
+  final Animation<double>? animation;
+
+  const QutationRetrive({Key? key, this.animationController, this.animation})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animationController!,
+      builder: (BuildContext context, Widget? child) {
+        return FadeTransition(
+          opacity: animation!,
+          child: new Transform(
+            transform: new Matrix4.translationValues(
+                0.0, 30 * (1.0 - animation!.value), 0.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 24, right: 24, top: 0, bottom: 0),
+                  child: Stack(
+                    clipBehavior: Clip.none,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16, bottom: 16),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: ProcurementAppTheme.white,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(8.0),
+                                bottomLeft: Radius.circular(8.0),
+                                bottomRight: Radius.circular(8.0),
+                                topRight: Radius.circular(8.0)),
+                            boxShadow: <BoxShadow>[
+                              BoxShadow(
+                                  color:
+                                      ProcurementAppTheme.grey.withOpacity(0.4),
+                                  offset: Offset(1.1, 1.1),
+                                  blurRadius: 10.0),
+                            ],
+                          ),
+                          child: Stack(
+                            alignment: Alignment.topLeft,
+                            children: <Widget>[
+                              ClipRRect(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8.0)),
+                                child: SizedBox(
+                                  height: 74,
+                                  child: AspectRatio(
+                                    aspectRatio: 1.714,
+                                    child: Image.asset("assets/app/back.png"),
+                                  ),
+                                ),
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: const EdgeInsets.only(
+                                          left: 150,
+                                          right: 30,
+                                          top: 25,
+                                        ),
+                                        child: Text(
+                                          "Quotation Requests",
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            fontFamily:
+                                                ProcurementAppTheme.fontName,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 14,
+                                            letterSpacing: 0.0,
+                                            color: ProcurementAppTheme
+                                                .nearlyDarkBlue,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                      left: 100,
+                                      bottom: 12,
+                                      top: 4,
+                                      right: 16,
+                                    ),
+                                    child: Text(
+                                      "!",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontFamily:
+                                            ProcurementAppTheme.fontName,
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 10,
+                                        letterSpacing: 0.0,
+                                        color: ProcurementAppTheme.grey
+                                            .withOpacity(0.5),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: -16,
+                        left: 0,
+                        child: SizedBox(
+                          width: 110,
+                          height: 110,
+                          child: Image.asset("assets/images/feedbackImage.png"),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+}
