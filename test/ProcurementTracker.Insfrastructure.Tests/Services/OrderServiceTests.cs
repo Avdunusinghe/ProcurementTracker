4a067b7e96c9345913a7a7605efc7b3d6211880f
Avdunusinghe <avdunusinghe@gmail.com>
2022-11-06 06:12:17 +0530

    Unit testing Implemented

--- test/ProcurementTracker.Insfrastructure.Tests/Services/OrderServiceTests.cs
+++ test/ProcurementTracker.Insfrastructure.Tests/Services/OrderServiceTests.cs
@@ -5,8 +5,13 @@ using Moq;
 using ProcurementTracker.Application.Common.Interfaces;
 using ProcurementTracker.Application.Common.Response.OrderDTOs;
 using ProcurementTracker.Application.Common.Response.OrderItemDTOs;
+using ProcurementTracker.Application.Common.Response.PurchaseRequestDTOs;
 using ProcurementTracker.Application.Orders.Command;
 using ProcurementTracker.Application.Orders.Query;
+using ProcurementTracker.Application.PurchaseRequests.Command;
+using ProcurementTracker.Application.PurchaseRequests.Query;
+using ProcurementTracker.Application.PurchaseRequestsItems.Command;
+using ProcurementTracker.Application.PurchaseRequestsItems.Query;
 using ProcurementTracker.Domain.Entities;
 using ProcurementTracker.Infrastructure.Services;
 using ProcurementTracker.WebAPI.Services;
@@ -193,9 +198,193 @@ namespace ProcurementTracker.Infrastructure.Services.Tests
         }
 
         [TestMethod()]
-        public async Task SavePurchaseRequestTest()
+        public async Task SavePurchaseRequestTest_Success()
         {
             var cancellationToken = new CancellationToken();
+
+            var purchaseRequestDTO = new PurchaseRequestDTO()
+            {
+                PurchaseRequestStatus = (Domain.Enums.PurchaseRequestStatus)1,
+                RequiredDeliveryDate = DateTime.Now,
+                Description = "Test",
+                SupplierId = 1,
+                TotalPrice = 750,
+
+            };
+
+            purchaseRequestDTO.PurchaseRequestProductItems.Add(new PurchaseRequestProductItemDTO()
+            {
+                 Id = 0,
+                 ProductId = 2,
+                 NumberOfItem = 1,
+                 
+            });
+
+            var purchaseRequest = new PurchaseRequest()
+            {
+                PurchaseRequestStatus = (Domain.Enums.PurchaseRequestStatus)1,
+                RequiredDeliveryDate = DateTime.Now,
+                Description = "test",
+                SupplierId = 1,
+                IsActive = true,
+                TotalPrice = 5600,
+                StatusChangedById = 1,
+
+            };
+
+            purchaseRequest.PurchaseRequestProductItems = new HashSet<PurchaseRequestProductItem>();
+
+            foreach (var item in purchaseRequestDTO.PurchaseRequestProductItems)
+            {
+                var purchaseRequestProductItem = new PurchaseRequestProductItem()
+                {
+                    ProductId = item.ProductId,
+                    NumberOfItem = item.NumberOfItem,
+                    PurchaseRequestId = purchaseRequest.Id
+                };
+
+                purchaseRequest.PurchaseRequestProductItems.Add(purchaseRequestProductItem);
+            }
+
+            var mediator = new Mock<IMediator>();
+            var currentUserService = new Mock<ICurrentUserService>();
+
+            mediator.Setup(x => x.Send(new GetPurchaseRequestByIdQuery(purchaseRequestDTO.Id), It.IsAny<CancellationToken>()))
+              .Returns(Task.FromResult<PurchaseRequest>(null));
+
+            var newPurchaseRequest = new PurchaseRequest()
+            {
+                PurchaseRequestStatus = (Domain.Enums.PurchaseRequestStatus)1,
+                RequiredDeliveryDate = DateTime.Now,
+                Description = "test",
+                SupplierId = 1,
+                IsActive = true,
+                TotalPrice = 5600,
+                StatusChangedById = 1,
+            };
+            purchaseRequest.PurchaseRequestProductItems = new HashSet<PurchaseRequestProductItem>();
+
+            foreach (var item in purchaseRequestDTO.PurchaseRequestProductItems)
+            {
+                var purchaseRequestProductItem = new PurchaseRequestProductItem()
+                {
+                    ProductId = item.ProductId,
+                    NumberOfItem = item.NumberOfItem,
+                    PurchaseRequestId = purchaseRequest.Id
+                };
+
+                purchaseRequest.PurchaseRequestProductItems.Add(purchaseRequestProductItem);
+            }
+
+            mediator.Setup(x => x.Send(new CreatePurchaseRequestCommand(), It.IsAny<CancellationToken>()))
+             .Returns(Task.FromResult<PurchaseRequest>(null));
+
+            var orderService = new OrderService(mediator.Object, currentUserService.Object);
+            var response = await orderService.SavePurchaseRequest(purchaseRequestDTO, cancellationToken);
+
+            Assert.IsTrue(response.IsSuccess);
+
+
+        }
+
+        [TestMethod()]
+        public async Task SavePurchaseRequestTest_Update_Success()
+        {
+            var cancellationToken = new CancellationToken();
+
+            var purchaseRequestDTO = new PurchaseRequestDTO()
+            {
+                PurchaseRequestStatus = (Domain.Enums.PurchaseRequestStatus)1,
+                RequiredDeliveryDate = DateTime.Now,
+                Description = "Test",
+                SupplierId = 1,
+                TotalPrice = 750,
+
+            };
+
+            purchaseRequestDTO.PurchaseRequestProductItems.Add(new PurchaseRequestProductItemDTO()
+            {
+                Id = 0,
+                ProductId = 2,
+                NumberOfItem = 1,
+
+            });
+
+            var purchaseRequest = new PurchaseRequest()
+            {
+                PurchaseRequestStatus = (Domain.Enums.PurchaseRequestStatus)1,
+                RequiredDeliveryDate = DateTime.Now,
+                Description = "test",
+                SupplierId = 1,
+                IsActive = true,
+                TotalPrice = 5600,
+                StatusChangedById = 1,
+
+            };
+
+            purchaseRequest.PurchaseRequestProductItems = new HashSet<PurchaseRequestProductItem>();
+
+            foreach (var item in purchaseRequestDTO.PurchaseRequestProductItems)
+            {
+                var purchaseRequestProductItem = new PurchaseRequestProductItem()
+                {
+                    ProductId = item.ProductId,
+                    NumberOfItem = item.NumberOfItem,
+                    PurchaseRequestId = purchaseRequest.Id
+                };
+
+                purchaseRequest.PurchaseRequestProductItems.Add(purchaseRequestProductItem);
+            }
+
+            var mediator = new Mock<IMediator>();
+            var currentUserService = new Mock<ICurrentUserService>();
+
+            mediator.Setup(x => x.Send(new GetPurchaseRequestByIdQuery(purchaseRequestDTO.Id), It.IsAny<CancellationToken>()))
+              .Returns(Task.FromResult<PurchaseRequest>(purchaseRequest));
+
+            var newPurchaseRequest = new PurchaseRequest()
+            {
+                PurchaseRequestStatus = (Domain.Enums.PurchaseRequestStatus)1,
+                RequiredDeliveryDate = DateTime.Now,
+                Description = "test",
+                SupplierId = 1,
+                IsActive = true,
+                TotalPrice = 5600,
+                StatusChangedById = 1,
+            };
+            purchaseRequest.PurchaseRequestProductItems = new HashSet<PurchaseRequestProductItem>();
+
+            foreach (var item in purchaseRequestDTO.PurchaseRequestProductItems)
+            {
+                var purchaseRequestProductItem = new PurchaseRequestProductItem()
+                {
+                    ProductId = item.ProductId,
+                    NumberOfItem = item.NumberOfItem,
+                    PurchaseRequestId = purchaseRequest.Id
+                };
+
+                purchaseRequest.PurchaseRequestProductItems.Add(purchaseRequestProductItem);
+            }
+
+            var purchaseRequestProductItemObj = new PurchaseRequestProductItem()
+            {
+                Id = 1,
+                NumberOfItem = 600,
+                PurchaseRequestId = 1
+            };
+
+            mediator.Setup(x => x.Send(new PurchaseRequestProductItemGetByRequestIdQuery(purchaseRequest.Id), It.IsAny<CancellationToken>()))
+             .Returns(Task.FromResult<PurchaseRequestProductItem>(purchaseRequestProductItemObj));;
+
+
+            mediator.Setup(x => x.Send(new CreatePurchaseRequestProductItemCommand() {PurchaseRequestProdcuctItem = purchaseRequest.PurchaseRequestProductItems.FirstOrDefault() }, It.IsAny<CancellationToken>()))
+            .Returns(Task.FromResult<PurchaseRequestProductItem>(null)); ;
+
+            var orderService = new OrderService(mediator.Object, currentUserService.Object);
+            var response = await orderService.SavePurchaseRequest(purchaseRequestDTO, cancellationToken);
+
+            Assert.IsTrue(response.IsSuccess);
+
 
         }
     }
